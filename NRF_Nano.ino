0fe4b49903fac9000fd58599c26984e5c4b7ae41
banovacivan <ibanovac.st@gmail.com>
2026-06-11 17:56:48 +0200

    Update NRF_Nano.ino

--- NRF_Nano.ino
+++ NRF_Nano.ino
@@ -8,14 +8,14 @@ const byte address[6] = "00001";
 void setup() {
   radio.begin();
   radio.openWritingPipe(address);
-  radio.setPALevel(RF24_PA_MIN); // Use _HIGH if you have stable power
+  radio.setPALevel(RF24_PA_MIN); 
   radio.stopListening();
 }
 
 void loop() {
   int potValue = analogRead(A0);
-  int angleValue = map(potValue, 0, 1023, 0, 180); // Map to servo degrees
+  int angleValue = map(potValue, 0, 1023, 0, 180); 
   
   radio.write(&angleValue, sizeof(angleValue));
-  delay(10); // Smooth out the transmission
+  delay(10); 
 }
