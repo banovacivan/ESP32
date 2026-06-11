3b9216e5392b35b171f92e7e46b04b67f013c7cb
banovacivan <ibanovac.st@gmail.com>
2026-06-11 17:57:23 +0200

    Update NRF_C3.ino

--- NRF_C3.ino
+++ NRF_C3.ino
@@ -3,18 +3,18 @@
 #include <RF24.h>
 #include <ESP32Servo.h>
 
-RF24 radio(2, 7); // CE, CSN pins for ESP32-C3
+RF24 radio(2, 7); // CE, CSN 
 const byte address[6] = "00001";
 Servo myServo;
 
 void setup() {
-  myServo.attach(3); // Signal on GPIO 3
+  myServo.attach(3); // GPIO 3
   
-  // Start SPI with C3 default pins: SCK=4, MISO=5, MOSI=6
+  // SPI : SCK=4, MISO=5, MOSI=6
   SPI.begin(4, 5, 6, 7); 
   
   if (!radio.begin()) {
-    // If radio fails to start, you can add a serial print here to debug
+    
   }
   
   radio.openReadingPipe(0, address);
@@ -28,7 +28,6 @@ void loop() {
     int receivedAngle = 0;
     radio.read(&receivedAngle, sizeof(receivedAngle));
     
-    // Safety check to ensure the value is within servo range
     if (receivedAngle >= 0 && receivedAngle <= 180) {
       myServo.write(receivedAngle);
     }
