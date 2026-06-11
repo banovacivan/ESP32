da48f39d24d9a55f03fa9a7dfabd4c35a3494794
banovacivan <ibanovac.st@gmail.com>
2026-06-11 17:54:46 +0200

    Update MQ3_alcohol.ino

--- MQ3_alcohol.ino
+++ MQ3_alcohol.ino
@@ -1,22 +1,16 @@
 #define PIN_ANALOG_IN 2
 int digitalPin =  15;
 
-//The following two variables hold the digital signal and adc values respectively
 int analogVal = 0;
 int adcVal = 0;
 
 void setup() {
   Serial.begin(9600);
-  pinMode(digitalPin, INPUT); //Digital pin 13 is set to input mode
+  pinMode(digitalPin, INPUT); 
 }
 
-//In loop()，the digitalRead()function is used to obtain the digital value,
-//the analogRead() function is used to obtain the ADC value. 
-//and then the map() function is used to convert the value into an 8-bit precision DAC value. 
-//The input and output voltage are calculated according to the previous formula, 
-//and the information is finally printed out.
 void loop() {
-  int digitalVal = digitalRead(digitalPin);  //Read digital signal;
+  int digitalVal = digitalRead(digitalPin);  //Read 
   int adcVal = analogRead(PIN_ANALOG_IN);
   int dacVal = map(adcVal, 0, 4095, 0, 255);
   double voltage = adcVal / 4095.0 * 3.3;
